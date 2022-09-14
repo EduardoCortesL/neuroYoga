7f01088b739b13f87ef55d0bf901927645fd3d8b
Eduardo CL <corteslalo97@gmail.com>
2022-09-13 20:18:39 -0700

    added video background and some cleanup

--- promac_code/pages/index.js
+++ promac_code/pages/index.js
@@ -12,10 +12,10 @@ export async function getStaticProps(){
   const overViewData = await res.json()
 
   //const res2 = await fetch('http://localhost:1337/api/verdeblocks/')
-  const res2 = await fetch('https://mighty-anchorage-30412.herokuapp.com/api/bit-overflow-mores/')
-  const moreData = await res2.json()
+  //const res2 = await fetch('https://mighty-anchorage-30412.herokuapp.com/api/bit-overflow-mores/')
+  //const moreData = await res2.json()
 
-  return { props: {overViewData, moreData}}
+  return { props: {overViewData}}
 }
 
 const Home = ({overViewData, moreData}) => {
@@ -34,25 +34,25 @@ const Home = ({overViewData, moreData}) => {
         X
           </div>
         : <div onClick={clickHandler} className="fixed bottom-5 right-5 z-30 text-white font-extrabold bg-black rounded-full p-4 opacity-75 cursor-pointer hover:opacity-100 hover:bg-yellow-300 hover:text-black">
-        Want to chat?
+        Contact Us
         </div>
       }
       
       <header className="relative flex items-center justify-center h-screen sm:mb-12 overflow-hidden">
-        <div className="absolute sm:left-10 top-10 z-30 text-white text-4xl font-bold">
-            BitOverflow
+        <div className="absolute sm:left-10 top-10 z-30 text-4xl font-bold">
+            Promac Engineering
         </div>
         <div className="grid grid-cols-1">
-          <div className="relative z-30 text-white text-6xl sm:text-9xl font-extrabold">
-              Built
+          <div className="relative z-30 text-6xl text-white sm:text-45l font-extrabold">
+              Your one stop shop
           </div>
-          <div className="relative z-30 text-white text-6xl sm:text-9xl font-extrabold">
-              Different.
+          <div className="relative z-30 text-6xl text-white sm:text-6l font-extrabold">
+              For all you consulting needs.
           </div>
         </div>
         
         <video autoPlay loop muted playsInline className="absolute z-10 w-auto min-w-full min-h-full max-w-none" >
-          <source src="/videos/background2-block-0.mp4" type="video/mp4" />
+          <source src="https://firebasestorage.googleapis.com/v0/b/bito-b4105.appspot.com/o/37427_construction_on_bridge_on_site_in_field_aerial_By_The_Stock_Studio_Artlist_HD.mp4?alt=media&token=90d1af61-c538-4cde-a084-2275181e8dde" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <link rel="icon" href="/favicon.ico" />
@@ -60,56 +60,16 @@ const Home = ({overViewData, moreData}) => {
 
       <main className="flex flex-col items-center justify-center sm:px-20 text-center">
         <div className="relative mt-6 sm:p-16 text-xl sm:text-6xl text-left">
-            <div className="text-black font-extrabold mt-2">
-              BitOverflow is the future.
-            </div>
-            <div className="text-black mt-2">
-              <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">EVERYTHING</span> <span className="underline"> hand-crafted</span>
-            </div>
-            <div className="text-black mt-2">
-              <span className="font-extrabold">EVERYTHING</span> <span className="text-transparent bg-clip-text bg-gradient-to-br from-nft-purple to-nft-cotton font-extrabold">in-house </span>
-            </div>
-            <div className="mt-2 font-extrabold text-3xl sm:text-8xl text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
-             Built Different.
-            </div>
+          <div>
+            For all your engineering needs <b>Promac</b> is here 
+          </div>
         </div>
-        <div className="w-full sm:w-5/6 my-14 pb-8 sm:p-8 text-sm sm:text-lg font-bold bg-gradient-to-br from-nft-sky to-nft-aqua">
+        <div className="w-full my-14 pb-8 sm:p-8 text-sm sm:text-lg font-bold bg-gradient-to-r from-yellow-500 to-orange-500">
             <div className="font-bold mt-2 text-xl sm:text-4xl text-white p-4">
                 Our Focus Areas
             </div>
             <div className="flex place-content-center cursor-pointer sm:font-extrabold ">
-              <div className="rounded-full py-2 sm:py-2 px-3 sm:px-5 m-2 bg-white">
-                <div className="text-black  hover:bg-gradient-to-br hover:to-nft-sky hover:from-nft-aqua hover:text-transparent hover:bg-clip-text">Sustainability</div>
-              </div>
-              <div className="rounded-full py-2 sm:py-2 px-3 sm:px-5 m-2 bg-white">
-              <div className="text-black  hover:bg-gradient-to-br hover:to-nft-sky hover:from-nft-aqua hover:text-transparent hover:bg-clip-text">Decentralization</div>
-              </div>
-            </div>
-            <div className="flex place-content-center cursor-pointer sm:font-extrabold">
-              <div className="rounded-full py-2 sm:py-2 px-3 sm:px-5 m-2 bg-white">
-                <div className="text-black  hover:bg-gradient-to-br hover:to-nft-sky hover:from-nft-aqua hover:text-transparent hover:bg-clip-text">Blockchain</div>
-              </div>
-              <div className="rounded-full py-2 sm:py-2 px-3 sm:px-5 m-2 bg-white">
-                <div className="text-black  hover:bg-gradient-to-br hover:to-nft-sky hover:from-nft-aqua hover:text-transparent hover:bg-clip-text">AI</div>
-              </div>
-              <div className="rounded-full py-2 sm:py-2 px-3 sm:px-5 m-2 bg-white">
-                <div className="text-black  hover:bg-gradient-to-br hover:to-nft-sky hover:from-nft-aqua hover:text-transparent hover:bg-clip-text">Health</div>
-              </div>        
-              <div className="rounded-full py-2 sm:py-2 px-3 sm:px-5 m-2 bg-white">
-                <div className="text-black  hover:bg-gradient-to-br hover:to-nft-sky hover:from-nft-aqua hover:text-transparent hover:bg-clip-text">Crypto</div>
-              </div>
-            </div>
-            <div className="flex place-content-center cursor-pointer sm:font-extrabold">
-              <div className="rounded-full py-2 sm:py-2 px-3 sm:px-5 m-2 bg-white">
-                <div className="text-black  hover:bg-gradient-to-br hover:to-nft-sky hover:from-nft-aqua hover:text-transparent hover:bg-clip-text">NFTs</div>
-              </div>
-              <div className="rounded-full py-2 sm:py-2 px-3 sm:px-5 m-2 bg-white">
-                <div className="text-black  hover:bg-gradient-to-br hover:to-nft-sky hover:from-nft-aqua hover:text-transparent hover:bg-clip-text">VR/AR</div>
-              </div>
-              <div className="rounded-full py-2 sm:py-2 px-3 sm:px-5 m-2 bg-white">
-                <div className="text-black  hover:bg-gradient-to-br hover:to-nft-sky hover:from-nft-aqua hover:text-transparent hover:bg-clip-text">Physics</div>
-              </div>
-            </div>   
+        </div>
         </div>
 
         <div className="flex relative mt-6 text-4xl sm:text-8xl">
@@ -118,7 +78,16 @@ const Home = ({overViewData, moreData}) => {
             </div>
         </div>
         <OverView data={overViewData.data}/>
-        <More data={moreData.data}/>
+        <div className="flex relative mt-6 text-4xl sm:text-8xl">
+        <div className="text-black font-extrabold mt-2">
+          About Us
+          </div>
+          <div>
+
+          </div>
+        </div>
+        
+
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
